18a944957e394be7f891d692072666c12e8626a9
Jacky Tran <tdhungit@gmail.com>
2021-11-25 11:36:21 +0700

    AP customize report

--- account_report_custom/models/saleorder_undelivered_report.py
+++ account_report_custom/models/saleorder_undelivered_report.py
@@ -118,6 +118,6 @@ class ReportSaleOrderUndelivered(models.Model):
 
     def _get_hierarchy_details(self, options):
         return [
-            self._hierarchy_level('order_id', foldable=True, namespan=1),
+            self._hierarchy_level('order_id', foldable=False, namespan=1),
             self._hierarchy_level('id'),
         ]
