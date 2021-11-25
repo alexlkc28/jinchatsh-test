b72da4f69970f1bcbb1a01126047864a590bba12
Jacky Tran <tdhungit@gmail.com>
2021-11-25 10:29:51 +0700

    AP customize report

--- account_report_custom/models/saleorder_undelivered_report.py
+++ account_report_custom/models/saleorder_undelivered_report.py
@@ -89,6 +89,9 @@ class ReportSaleOrderUndelivered(models.Model):
     @api.model
     def _get_column_details(self, options):
         columns = [
+            self._header_column(),
+            self._field_column('report_date'),
+
             self._field_column('order_no', name=_("Order No."), ellipsis=True),
             self._field_column('partner_name', name=_("Customer")),
             self._field_column('english_name', name=_("Customer English Name")),
