8ce2984d56a52c6a493eb5bca30900bafcccc297
Jacky Tran <tdhungit@gmail.com>
2021-11-26 14:27:54 +0700

    customize

--- jinchatsh_custom/models/jinchatsh_ir_attachment.py
+++ jinchatsh_custom/models/jinchatsh_ir_attachment.py
@@ -13,7 +13,6 @@ class JinchatshIrAttachment(models.Model):
 
     @api.model
     def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
-        order = 'create_date asc'
-        search = super().search_read(domain, fields=fields, offset=offset, limit=limit, order=order)
-        _logger.info(search)
-        return search
+        if order is None:
+            order = 'create_date desc'
+        return super().search_read(domain, fields=fields, offset=offset, limit=limit, order=order)
