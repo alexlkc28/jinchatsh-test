f4663eaba55c53a64032ea232b72bcaf91857823
Jacky Tran <tdhungit@gmail.com>
2021-11-26 16:54:03 +0700

    customize

--- jinchatsh_custom/static/src/js/attachment_viewer.js
+++ jinchatsh_custom/static/src/js/attachment_viewer.js
@@ -5,5 +5,7 @@ import Widget from 'web.Widget';
 import AttachmentViewer from '@mail_enterprise/js/attachment_viewer';
 
 var JinchatshAttachmentViewer = Widget.extend(AttachmentViewer, {
-
+    init: function (parent, attachments) {
+        this._super.apply(this, arguments);
+    },
 });
